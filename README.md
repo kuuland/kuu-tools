ac333ed66fc7401f724a0ca88855c14af682b69a
yinfxs <yinfxs@gmail.com>
2019-07-05 11:52:53 +0800

    feat: release 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
 # kuu-tools
+
 Kuu front-end tools
+
+## Request utils
+
+### GET
+
+```js
+import { get } from 'kuu-tools'
+
+const data = await get('/member/get?foo=1&bar=2')
+```
+
+### POST
+
+```js
+import { post } from 'kuu-tools'
+
+const data = await post('/member/signup', { user: 'foo', name: 'bar' })
+```
+
+### PUT
+
+```js
+import { put } from 'kuu-tools'
+
+const data = await put('/member/signup?foo=1&bar=2', { user: 'foo', name: 'bar' })
+```
+
+### DELETE
+
+```js
+import { del } from 'kuu-tools'
+
+const data = await del('/member/signup?foo=1&bar=2', { user: 'foo', name: 'bar' })
+```
+
+## Model SDK
+
+### Create
+
+> https://github.com/kuuland/kuu#create-record
+
+```js
+import { create } from 'kuu-tools'
+
+// create a role
+const data = await create('Role', { Code: 'foo', Name: 'bar' })
+
+// batch create roles
+const data = await create('Role', [{ Code: 'foo1', Name: 'bar1' }, { Code: 'foo2', Name: 'bar2' }])
+```
+
+### Update
+
+> https://github.com/kuuland/kuu#update-fields
+
+```js
+import { update } from 'kuu-tools'
+
+// update a param
+const data = await update('param', { ID: 5 }, { Value: 'new value', Name: 'foobar' })
+
+// batch updates
+const data = await update('param', { ID: { $in: [5, 10, 11] } }, { Value: 'new value' }, true)
+```
+
+### Delete
+
+> https://github.com/kuuland/kuu#delete-record
+
+```js
+import { remove } from 'kuu-tools'
+
+// delete a param
+const data = await remove('param', { ID: 5 })
+
+// batch delete
+const data = await remove('param', { ID: { $in: [5, 10, 11] } }, true)
+
+// batch and unsoft delete
+const data = await remove('param', { ID: { $in: [5, 10, 11] } }, true, true)
+```
+
+### Query
+
+> https://github.com/kuuland/kuu#query
+
+```js
+import { list, one, id } from 'kuu-tools'
+
+// list query
+const data = await list('param', { cond: { ID: { $in: [5, 10, 11] } }, page: 3, sort: '-CreatedAt' })
+
+// query only one record
+const data = await one('param', { cond: { Name: 'foo' } })
+
+// query by id
+const data = await id('param', 11)
+
+```
+
+## i18n
+
+```js
+import { L } from 'kuu-tools'
+
+window.i18nMessages = {
+     "hello": "你好",
+     "welcome": "欢迎 {{name}}"
+}
+
+// Usage: L(id, defaultMessage, formattedContext)
+
+L('hello', 'Hello')                             // => 你好
+L('not_found', 'Not found')                     // => Not found
+L('welcome', 'Welcome Kuu', { name: 'Kuu' })    // => 欢迎 Kuu
+```
+
+> Notes: Message supports the [mustache](https://github.com/janl/mustache.js) syntax.
---
+++ src/index.js
@@ -0,0 +1,375 @@
+import fetch from 'isomorphic-fetch'
+import { message } from 'antd'
+import _ from 'lodash'
+import qs from 'qs'
+
+const configs = {
+  prefix: '/api'
+}
+
+/**
+ * 配置函数
+ * @param [opts] 配置项（可选）
+ * @returns 合并后的配置项
+ */
+export function config (opts) {
+  _.merge(configs, opts)
+  return configs
+}
+
+/**
+ * 底层请求封装
+ * @param url
+ * @param body
+ * @param [opts]
+ * @return {Promise}
+ */
+async function request (url, opts) {
+  // url加工
+  url = withPrefix(url)
+  // 配置加工
+  opts = _.merge({
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    cache: 'no-cache',
+    credentials: 'include'
+  }, opts)
+  if (_.isFunction(_.get(configs, 'beforeFetch'))) {
+    const args = { url, opts }
+    const ret = _.get(configs, 'beforeFetch')(args)
+    if (ret) {
+      url = ret.url
+      opts = ret.opts
+    } else {
+      url = args.url
+      opts = args.opts
+    }
+  }
+  const onError = _.result(opts, 'onError', null)
+  delete opts.onError
+
+  // 定义异常处理器
+  const errorHandler = async (json) => {
+    const msg = _.get(json, 'msg') || window.L('网络异常')
+    if (onError !== null) {
+      if (_.isFunction(onError)) {
+        onError(msg)
+      }
+    } else {
+      message.error(msg)
+    }
+  }
+
+  // 执行请求
+  const res = await fetch(url, opts)
+  let data = null
+  if (res.status >= 200 && res.status < 300) {
+    const json = await res.json()
+    if (opts.rawData) {
+      data = json
+    } else {
+      if (json.code === 555) {
+        if (url !== `${_.get(configs, 'prefix', '')}/logout`) {
+          window.g_app._store.dispatch({
+            type: 'user/logout'
+          })
+        }
+      } else if (json.code !== 0) {
+        console.error(json)
+        await errorHandler(json)
+      } else {
+        data = _.get(json, 'data', json)
+      }
+    }
+  } else {
+    await errorHandler()
+  }
+  return data
+}
+
+/**
+ * 文件下载
+ * @param {string} url
+ * @param {object} options
+ */
+export async function donwloadFile (url, options) {
+  const res = await fetch(url, _.merge({
+    cache: 'no-cache',
+    credentials: 'include'
+  }, options))
+  const blob = await res.blob()
+  const a = window.document.createElement('a')
+  const href = window.URL.createObjectURL(blob)
+  const filename = new RegExp('filename="(.*)"').exec(res.headers.get('Content-Disposition'))[1]
+  a.href = href
+  a.download = filename
+  a.click()
+  window.URL.revokeObjectURL(url)
+}
+
+/**
+ * 常规GET请求
+ * @param url
+ * @param [opts]
+ * @return {Promise}
+ */
+export function get (url, opts) {
+  return request(url, opts)
+}
+
+/**
+ * 常规POST请求
+ * @param url
+ * @param body
+ * @param [opts]
+ * @return {Promise}
+ */
+export function post (url, body, opts) {
+  return request(url, _.merge({
+    method: 'POST',
+    body: JSON.stringify(body)
+  }, opts))
+}
+
+/**
+ * 常规PUT请求
+ * @param url
+ * @param body
+ * @param [opts]
+ * @return {Promise}
+ */
+export function put (url, body, opts) {
+  return request(url, _.merge({
+    method: 'PUT',
+    body: JSON.stringify(body)
+  }, opts))
+}
+
+/**
+ * 常规DELETE请求
+ * @param url
+ * @param body
+ * @param [opts]
+ * @return {Promise}
+ */
+export function del (url, body, opts) {
+  return request(url, _.merge({
+    method: 'DELETE',
+    body: JSON.stringify(body)
+  }, opts))
+}
+
+/**
+ * 给接口地址添加通用前缀
+ * @param url
+ * @returns {string}
+ */
+export function withPrefix (url) {
+  let prefix = _.get(configs, 'prefix', '')
+  if (prefix) {
+    prefix = prefix.startsWith('/') ? prefix : `/${prefix}`
+    prefix = prefix.endsWith('/') ? prefix.substring(0, prefix.length - 1) : prefix
+  }
+  url = `${prefix}${url}`.toLowerCase()
+  return url
+}
+
+/**
+ * SDK根据名称获取最终的地址
+ * @param name
+ * @returns {string}
+ */
+export function getModelUrl (name) {
+  return `/${name}`.toLowerCase()
+}
+
+/**
+ * 默认新增接口
+ * @param name
+ * @param data
+ * @returns {Promise<{}|*>}
+ */
+export async function create (name, data) {
+  if (!name) {
+    throw new Error(`name can not be empty: ${name}`)
+  }
+  const url = getModelUrl(name)
+  if (_.isEmpty(data)) {
+    return {}
+  }
+  try {
+    const arr = Array.isArray(data) ? data : [data]
+    const json = await post(url, arr)
+    if (Array.isArray(data)) {
+      return _.get(json, '[0]', json)
+    }
+    return json
+  } catch (error) {
+    console.error(error)
+    return {}
+  }
+}
+
+/**
+ * 默认删除接口
+ * @param name
+ * @param cond
+ * @returns {Promise<{}>}
+ */
+export async function remove (name, cond, multi = false, unsoft = false) {
+  if (!name) {
+    throw new Error(`name can not be empty: ${name}`)
+  }
+  const url = getModelUrl(name)
+  if (_.isEmpty(cond)) {
+    return {}
+  }
+  try {
+    const json = await del(url, { cond, multi, unsoft })
+    return json
+  } catch (error) {
+    console.error(error)
+    return {}
+  }
+}
+
+/**
+ * 默认修改接口
+ * @param name
+ * @param cond
+ * @param doc
+ * @returns {Promise<{}>}
+ */
+export async function update (name, cond, doc, multi = false) {
+  if (!name) {
+    throw new Error(`name can not be empty: ${name}`)
+  }
+  const url = getModelUrl(name)
+  if (_.isEmpty(cond) || _.isEmpty(doc)) {
+    return {}
+  }
+  try {
+    const json = await put(url, { cond, doc, multi })
+    return json
+  } catch (error) {
+    console.error(error)
+    return {}
+  }
+}
+
+/**
+ * 默认列表查询接口
+ * @param name
+ * @param query
+ * @returns {Promise<{}|*>}
+ */
+export async function list (name, query = {}) {
+  if (!name) {
+    throw new Error(`name can not be empty: ${name}`)
+  }
+  let url = getModelUrl(name)
+  if (_.isPlainObject(query.cond)) {
+    query.cond = JSON.stringify(query.cond)
+  } else if (!_.isString(query.cond)) {
+    query.cond = {}
+  }
+  try {
+    url = `${url}?${qs.stringify(query)}`
+    const json = await get(url)
+    const data = _.get(json, 'data') || json
+    if (_.has(data, 'list') && !Array.isArray(data.list)) {
+      data.list = []
+    }
+    return data
+  } catch (error) {
+    console.error(error)
+    return {}
+  }
+}
+
+/**
+ * 默认单个查询接口（基于列表接口）
+ * @param name
+ * @param cond
+ * @returns {Promise<*|{}>}
+ */
+export async function one (name, query = {}) {
+  const json = await list(name, query)
+  return _.get(json, 'list[0]') || {}
+}
+
+/**
+ * 默认ID查询接口（基于列表接口）
+ * @param name
+ * @param idVal
+ * @returns {Promise<*|{}>}
+ */
+export async function id (name, idVal) {
+  const idKey = _.get(configs, 'idKey', 'ID')
+  const json = await list(name, { cond: { [idKey]: idVal } })
+  return _.get(json, 'list[0]') || {}
+}
+
+/**
+ * 查询字典
+ * @param codeOrObject 字典编码或字典对象
+ * @returns {Promise<{}|*>}
+ */
+export async function getDict (codeOrObject) {
+  const obj = _.isPlainObject(codeOrObject) ? codeOrObject : { Code: codeOrObject }
+  if (_.isEmpty(obj)) {
+    console.error(window.L(`查询条件不能为空`))
+    return {}
+  }
+  const json = await list('dict', {
+    cond: obj,
+    page: 1,
+    size: 1,
+    sort: '-UpdatedAt'
+  })
+  const dict = _.get(json, 'list[0]', {})
+  return dict
+}
+
+/**
+ * 查询参数
+ * @param codeOrObject 参数编码或参数对象
+ * @returns {Promise<{}|*>}
+ */
+export async function getParam (codeOrObject) {
+  const obj = _.isPlainObject(codeOrObject) ? codeOrObject : { Code: codeOrObject }
+  if (_.isEmpty(obj)) {
+    console.error(window.L(`查询条件不能为空`))
+    return {}
+  }
+  const json = await list('param', {
+    cond: obj,
+    page: 1,
+    size: 1,
+    sort: '-UpdatedAt'
+  })
+  const dict = _.get(json, 'list[0]', {})
+  return dict
+}
+
+/**
+ * 国际化函数
+ * @param {*} key 字符串键
+ * @param {*} defaultMessage 默认值
+ * @param {*} context 参数
+ */
+export function L (key, defaultMessage, context) {
+  const language = _.result(window, 'g_app._store.getState.user.language') || _.result(window, 'i18nMessages', {})
+  const template = _.get(language, key, defaultMessage) || key
+  if (context && !_.isEmpty(context)) {
+    _.templateSettings.interpolate = /{{([\s\S]+?)}}/g
+    const compiled = _.template(template)
+    const value = compiled(context)
+    return value
+  }
+  return template
+}
+
+export default request
